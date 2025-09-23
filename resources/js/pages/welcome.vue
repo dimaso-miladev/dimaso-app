b19f2c7f2e618c3a13fc9efda8c7b689883575d3
miladev.net <miladev.net@gmail.com>
2025-09-23 14:42:41 +0000

    revert file welcome

---
+++ resources/js/pages/welcome.vue
@@ -0,0 +1,32 @@
+<template>
+<h1>App main</h1>  
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import ContactForm from '~/components/ContactForm';
+
+export default {
+  layout: 'basic',
+  components: {
+    ContactForm
+  },
+  metaInfo() {
+    return { title: this.$t('home') }
+  },
+
+  data: () => ({
+    title: window.config.appName,
+    heroBanner: '/assets/images/hero_banner.png',
+    iconDimaso: '/assets/images/icon-dimaso.png',
+    logoUrl: '/assets/images/dimaso_main_logo.webp',
+    iconPerson: '/assets/images/icPersonAdvertising.svg',
+    iconClick: '/assets/images/icParkSolidClick.svg',
+    iconLocation: '/assets/images/icLocationAdvetising.svg',
+  }),
+
+  computed: mapGetters({
+    authenticated: 'auth/check'
+  })
+}
+</script>
