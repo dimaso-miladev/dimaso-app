9e79ae05a0fae99d2e3234cd56efcbf5bef77424
miladev.net <miladev.net@gmail.com>
2025-09-23 09:52:18 +0000

    update UI

--- resources/js/layouts/basic.vue
+++ resources/js/layouts/basic.vue
@@ -1,21 +1,13 @@
 <template>
   <div class="dimaso-kit-79">
     <div id="page-wrapper" class="page-wrapper">
-    <HeaderClient/>
     <child />
-    <FooterClient/>
     </div>
   </div>
 </template>
 
 <script>
-import HeaderClient from '~/components/HeaderClient';
-import FooterClient from '~/components/FooterClient';
 export default {
-  components: {
-    HeaderClient,
-    FooterClient
-  },
   name: 'BasicLayout',
   data() {
     return {
